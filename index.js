70d38cce811f3588957709913a514ed6626fd76f
musil <musil@porg.cz>
2019-09-23 17:22:24 +0200

    servřík 2.0

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const http = require("http");
 
+let citac = 0;
+
 http.createServer((req, res) => {
+    if (req.url == "/") {
+        citac++;
+    }
     res.writeHead(200, {"Content-type": "text/html"});
-    res.end("<html><body>cusbus</body></html>");
+    res.end("<html lang='cs'><head><meta charset = 'UTF8'></head><body>Počet volání: " +citac + "</body></html>");
 }).listen(150);
 
