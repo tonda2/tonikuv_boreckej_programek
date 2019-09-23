8348273ee56742c4ddd413c50fc6049224573649
musil <musil@porg.cz>
2019-09-23 17:27:55 +0200

    servřík 2.0

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ http.createServer((req, res) => {
     if (req.url == "/") {
         citac++;
     }
-    res.writeHead(200, {"Content-type": "text/html"});
-    res.end("<html lang='cs'><head><meta charset = 'UTF8'></head><body>Počet volání: " +citac + "</body></html>");
+    if (req.url == "/jinastranka") {
+        res.writeHead(200, {"Content-type": "text/html"});
+        res.end("<html lang='cs'><head><meta charset = 'UTF8'></head><body>objevils super tajnou wwebku kamo</body></html>");
+    }
+    else{
+        res.writeHead(200, {"Content-type": "text/html"});
+        res.end("<html lang='cs'><head><meta charset = 'UTF8'></head><body>Počet volání: " +citac + "</body></html>");
+    }
 }).listen(150);
 
