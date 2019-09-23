c7c9426d82cdf1cbff3f64f4d581f9835097b42a
musil <musil@porg.cz>
2019-09-23 17:42:46 +0200

    servřík 2.0

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 
 let citac = 0;
+let druhejcitac = 0;
 
 http.createServer((req, res) => {
     if (req.url == "/") {
@@ -9,6 +10,21 @@ http.createServer((req, res) => {
     if (req.url == "/jinastranka") {
         res.writeHead(200, {"Content-type": "text/html"});
         res.end("<html lang='cs'><head><meta charset = 'UTF8'></head><body>objevils super tajnou wwebku kamo</body></html>");
+    }
+    else if (req.url == "/jsondemo"){
+        res.writeHead(200, {"Content-type": "application/json"});
+        let obj = {};
+        obj.jmeno = "Martin";
+        obj.prijmeni = "Borec";
+        obj.rokNarozeni = 1847;
+        res.end(JSON.stringify(obj));
+    }
+    else if (req.url == "/jsoncitac"){
+        res.writeHead(200, {"Content-type": "application/json"});
+        let pocet = {};
+        druhejcitac ++;
+        pocet.cislo = druhejcitac;
+        res.end(JSON.stringify(pocet));
     }
     else{
         res.writeHead(200, {"Content-type": "text/html"});
