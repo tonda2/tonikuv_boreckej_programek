866c03db3312843948a5b9e36d57163df5a92a9d
musil <musil@porg.cz>
2019-09-30 17:24:00 +0200

    datum, cas a dateformat

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const http = require("http");
+const dateFormat = require("dateformat");
+
+const DNY_V_TYDNU = ["Neděle", "Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek", "Sobota"];
 
 let citac = 0;
 let druhejcitac = 0;
@@ -26,9 +29,20 @@ http.createServer((req, res) => {
         pocet.cislo = druhejcitac;
         res.end(JSON.stringify(pocet));
     }
+    else if (req.url == "/denvtydnu"){
+        res.writeHead(200, {"Content-type": "application/json", "Access-Control-Allow-Origin":"*"});
+        let d = new Date();
+        let obj = {};
+        obj.systDatum = d;
+        obj.denVTydnuCiselne = d.getDay();
+        obj.denVTydnuCesky = DNY_V_TYDNU[d.getDay()] + " ";
+        obj.datumCesky = d.getDate() + "." + (d.getMonth()+1) + "." + d.getFullYear();
+        obj.datumCeskyFormat = dateFormat(d, "dd.mm.yyyy");
+        obj.casCesky = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+        res.end(JSON.stringify(obj));
+    }
     else{
         res.writeHead(200, {"Content-type": "text/html"});
         res.end("<html lang='cs'><head><meta charset = 'UTF8'></head><body>Počet volání: " +citac + "</body></html>");
     }
 }).listen(150);
-
