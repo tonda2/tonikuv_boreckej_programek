ebf3dc664ab0fb87dfb8fea5ecfba1bb572a18af
musil <musil@porg.cz>
2019-11-12 09:10:08 +0100

    konečně funguje správně porovnávání porovnává se v js, ne v html

--- api-cislo.js
+++ api-cislo.js
@@ -4,20 +4,21 @@ let nc;
 exports.apicislo = function(req, res) {
     let q = url.parse(req.url, true);
     res.writeHead(200, {"Content-type": "application/json", "Access-Control-Allow-Origin": "*"});
-    let obj = {};
-    const MIN_NC = 5;
-    const MAX_NC = 10;
-    nc = Math.random();
-    nc = MIN_NC + Math.trunc((MAX_NC-MIN_NC+1)*nc);
-    obj.nahodnecislo = nc;
-    res.end(JSON.stringify(obj));
-
-    if (q.pathname == "/cislo/zkontroluj"){
+    if (q.pathname == "/cislo") {
+        let obj = {};
+        const MIN_NC = 5;
+        const MAX_NC = 10;
+        nc = Math.random();
+        nc = MIN_NC + Math.trunc((MAX_NC - MIN_NC + 1) * nc);
+        obj.nahodnecislo = nc;
+        res.end(JSON.stringify(obj));
+    }
+    else if (q.pathname == "/cislo/zkontroluj"){
         res.writeHead(200, {"Content-type": "application/json"});
         let obj = {};
         obj.rozhodnuti = "špatně";
         obj.cislo = q.query["c"];
-        if (obj.cislo === nc){
+        if (obj.cislo == nc){
             obj.rozhodnuti = "správně";
         }
         else {
